af232b70aa923c16e661bbdeae6220ceab239b2e
Simone <simone.pagotto.kg@gmail.com>
2020-11-10 16:01:17 +0100

    aggiunti methods

--- jsnack-{3}/bonus/bonus.js
+++ jsnack-{3}/bonus/bonus.js
@@ -1,14 +1,18 @@
 // Creare un oggetto che rappresenti un triangolo rettangolo,
 // con le seguenti proprietà: base e altezza.
 // Calcolare perimetro e area.
+//Implementando methods
 
 var rightTriangle = {
   'base' : 5,
   'high' : 5,
   'area' : function() {
-
     return (this.base * this.high) / 2;
+  },
+  'perimeter' : function() {
+    return (this.base + this.high + ((this.base) ** 2 + (this.high) ** 2) ** .5);
   }
 };
 
-console.log(rightTriangle);
+console.log(rightTriangle.area());
+console.log(rightTriangle.perimeter());
